cb8fb1153cc236e080debc58de463b281b2be254
Toni <antonioinsa@tutanota.com>
2023-09-24 19:18:31 +0200

    Javascript actualizado, se dota de dinamismo a la videoconsola

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 let pantalla = document.getElementById("pantallavisible");
+let ledverde = document.getElementById("luzverde")
+let contador = 0;
 
 document.addEventListener("click", (e)=>{
 
@@ -7,11 +9,16 @@ document.addEventListener("click", (e)=>{
 
         pantalla.style.backgroundImage = "url('img/gif.gif')";
 
+        if (contador = 1) {ledverde.style.background = 'green'};
+    
+
 
     } else if (e.target.id === 'off') {
 
-        pantalla.style.backgroundImage = "url('img/playingwithpower.gif')";
-        
+        pantalla.style.backgroundImage = "url('img/marioaplauso.gif')";
+        contador++
+        if (contador >1) {ledverde.style.background = "linear-gradient(to bottom, rgb(134, 130, 130), rgb(175, 165, 165))"};
+    
     };
 
 });
