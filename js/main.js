c58322267926784ecdda928123616f2073284926
Toni <antonioinsa@tutanota.com>
2023-09-24 21:21:46 +0200

    Se añade funcionalidad al boton A y se enciende el led de carga, color naranja

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
 
 let pantalla = document.getElementById("pantallavisible");
-let ledverde = document.getElementById("luzverde")
+let ledverde = document.getElementById("luzverde");
+let ledambar = document.getElementById("luzambar");
 let contador = 0;
+
 
 document.addEventListener("click", (e)=>{
 
     if (e.target.id === 'on') {
 
-        pantalla.style.backgroundImage = "url('img/gif.gif')";
+        pantalla.style.backgroundImage = "url('img/startup.mp4')";
+       
 
         if (contador = 1) {ledverde.style.background = 'green'};
     
 
+    } else if (e.target.id === 'off') {
+
+        pantalla.style.backgroundImage = "url('img/mariogorrafin.gif')";
+        contador++;
+        if (contador >1) {ledverde.style.background = "linear-gradient(to bottom, rgb(134, 130, 130), rgb(175, 165, 165))"};
+        
+    };
+
+
+});
+
+document.addEventListener("click", (e)=>{
+
+    if (e.target.id === 'a') {
+
+        pantalla.style.backgroundImage = "url('img/startup.mp4')";
+       
+
+        if (contador = 1) {ledambar.style.background = 'orange'};
+    
 
     } else if (e.target.id === 'off') {
 
-        pantalla.style.backgroundImage = "url('img/marioaplauso.gif')";
-        contador++
-        if (contador >1) {ledverde.style.background = "linear-gradient(to bottom, rgb(134, 130, 130), rgb(175, 165, 165))"};
-    
+        contador++;
+        if (contador >1) {ledambar.style.background = "linear-gradient(to bottom, rgb(134, 130, 130), rgb(175, 165, 165))"};
+        
     };
+
 
 });
